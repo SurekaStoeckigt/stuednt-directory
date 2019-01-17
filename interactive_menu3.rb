4aaa8f9c6eb375b80750556c6805553e9ba66fb9
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-01-17 18:22:04 +0000

    Adding load students to interactive menu

--- interactive_menu3.rb
+++ interactive_menu3.rb
@@ -13,6 +13,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" #because we will add more items
 end
 
@@ -31,6 +32,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+        load_students
     when "9"
       exit
     else
@@ -59,7 +62,24 @@ def save_students
   file.close
 end
 
-
+def load_students
+  #open file for reading
+  file = File.open("students.csv", "r")
+  #read all lines into array and iterate over it
+  file.readlines.each do |line|
+      # discard the training new line character from line and split with comma
+      # assign the line to name and cohort variables
+      # parallel assignment: assigning two variables at the same time. 
+      # If assigned value is an array, the first variable gets first value of array and the second variable gets second value
+      name, cohort = line.chomp.split(",")
+      # once we have name and cohort, create new hash and put list of students into it.
+      # shovel operator
+      # different from input_students method because we are converting a string that we read from a file to a symbol
+      @students << {name: name, cohort: cohort.to_sym}
+  end
+  #close file
+  file.close
+end
 #nothing happens until we call the methods
 @students = interactive_menu
 print_header
